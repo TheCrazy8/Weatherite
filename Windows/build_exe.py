36f7a1ea57633e1964109ab27da07ae7b2cfbf23
TheCrazy8 <bennettbashford@gmail.com>
2025-09-09 15:17:14 -0400

    ...

---
+++ Windows/build_exe.py
@@ -0,0 +1,19 @@
+# build_exe.py
+"""
+Script to build a one-file executable for the Weatherite Windows app using PyInstaller.
+Run this script in PowerShell:
+    python build_exe.py
+"""
+
+import PyInstaller.__main__
+
+# Main script to package
+main_script = "main.py"
+
+print("Running PyInstaller...")
+PyInstaller.__main__.run([
+    main_script,
+    '--onefile',
+    '--name', 'Weatherite'
+])
+print("Build complete. Check the 'dist' folder for Weatherite.exe.")
